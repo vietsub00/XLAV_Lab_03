f177112b3fb66e7d0ecf538ce450a8a51a8bdfde
vietsub00 <49641797+vietsub00@users.noreply.github.com>
2020-12-25 10:53:42 +0700

    Cập nhật/chỉnh sửa comment

--- Blur.cpp
+++ Blur.cpp
@@ -7,13 +7,13 @@ int Blur::BlurImage(const Mat& sourceImage, Mat& destinationImage, int kWidth, i
 	int kernelSize = kWidth * kHeight;
 	vector<float> kernel;
 	switch (method) {
-	case MEAN: { //Mean
+	case MEAN: { //Toán tử trung bình
 		for (int i = 0; i < kernelSize; i++) {
 			kernel.push_back(1);
 		}
 		break;
 	}
-	case MEDIAN: { //Median (không dùng được convolution)
+	case MEDIAN: { //Toán tử trung vị (không dùng được convolution)
 		int nrow = sourceImage.rows;
 		int ncol = sourceImage.cols;
 		int nChannels = sourceImage.channels();
@@ -21,25 +21,24 @@ int Blur::BlurImage(const Mat& sourceImage, Mat& destinationImage, int kWidth, i
 
 		vector<uchar> medianKernel(kernelSize);
 
-		if (sourceImage.channels() == 1)
-			destinationImage = Mat(nrow, ncol, CV_8UC1);
-		else
-			destinationImage = Mat(nrow, ncol, CV_8UC3);
+		destinationImage = Mat(nrow, ncol, sourceImage.type());
 
 		uint8_t* pSrcData = (uint8_t*)sourceImage.data;
 		uint8_t* pDstData = (uint8_t*)destinationImage.data;
-
+		
+		//Duyệt qua các pixel trong ảnh đích
 		for (int cn = 0; cn < nChannels; cn++) {
 			for (int i = 0; i < nrow; i++) {
 				for (int j = 0; j < ncol; j++) {
 					int index = i * ncol * nChannels + j * nChannels + cn;
+					//Duyệt qua các pixel trong ảnh nguồn tương ứng với vị trí trong kernel
 					for (int k = 0; k < kernelSize; k++) {
 						int row = (k / kWidth) - (kHeight / 2);
 						int col = (k % kWidth) - (kWidth / 2);
 						//Xử lý kernel của pixel ở cạnh bằng cách lấy đối xứng qua pixel đó
 						if (i + row < 0 || i + row >= nrow) row = -row;
 						if (j + col < 0 || j + col >= ncol) col = -col;
-
+						
 						medianKernel[k] = pSrcData[index + row * ncol * nChannels + col * nChannels];
 					}
 					//sắp xếp các phần tử trong kernel 
@@ -51,7 +50,7 @@ int Blur::BlurImage(const Mat& sourceImage, Mat& destinationImage, int kWidth, i
 		}
 		return 0;
 	}
-	case GAUSSIAN: { //Gaussian
+	case GAUSSIAN: { //Gaussian kernel
 		float sigma = 1.0;
 		float r, s = 2.0 * sigma * sigma;
 		float sum = 0.0;
